5990c840b8946a2434115c3f608d18b52b1f7955
mai kawaoka <mai.kawaoka@seattleconsulting.co.jp>
2020-04-07 22:30:20 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+  <span>XYZ</span>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld2 />
   </div>
 </template>
 
