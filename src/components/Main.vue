e7fbc573b57f7163342126eb18290c340be19665
mai kawaoka <mai.kawaoka@seattleconsulting.co.jp>
2020-04-08 20:08:01 +0900

    【ポートフォリオ実装】20200408

---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="main Section">
+   <userName>Mai Kawaoka</userName>
+   <companyName>Seattle consulting</companyName>
+   <profilePicture>    </profilePicture>
+   </div>
+</template>
+
+
+
+<script>
+
+</script>
+
+
+
+<style scoped>
+  .main {
+      background-color: #333333;
+      height: 140px;
+      font-size: 20px;
+      color: #FFFFEE;
+      font-family: Sans-serif;
+  }
+</style>
